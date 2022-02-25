785be99c4651e7127896bf1156479b1cf2612080
Lin Liu <liulinn60@gmail.com>
2022-02-24 16:35:39 -0800

    firebase setup

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHAvq0Qx7THnRu0GUz0ZF-MBcA5zoVhGg",
+  authDomain: "house-marketplace-app-1ea4a.firebaseapp.com",
+  projectId: "house-marketplace-app-1ea4a",
+  storageBucket: "house-marketplace-app-1ea4a.appspot.com",
+  messagingSenderId: "877572029057",
+  appId: "1:877572029057:web:f5fe101ea09e806ee06b80"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
