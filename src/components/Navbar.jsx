b41f6b95fb5a50b4d069ec6f45e3d4fe6e8a4597
Lin Liu <liulinn60@gmail.com>
2022-02-26 18:47:39 -0800

    edit offers page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,7 +57,7 @@ function Navbar() {
                                     : 'navbarListItemName'
                             }
                         >
-                            Offer
+                            Offers
                         </p>
                     </li>
                     <li
