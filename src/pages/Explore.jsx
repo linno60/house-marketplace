a754fe4aa2badf6f840813f794b40badfa844b0b
Lin Liu <liulinn60@gmail.com>
2022-03-02 16:56:29 -0800

    adding slider to explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,34 +2,36 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+import Slider from '../components/Slider'
 
 function Explore() {
     return (
-        <div className="explore">
+        <div className='explore'>
             <header>
-                <p className="pageHeader">Explore</p>
+                <p className='pageHeader'>Explore</p>
             </header>
 
             <main>
                 {/* Slider */}
+                <Slider />
 
-                <p className="exploreCategoryHeading">Categories</p>
-                <div className="exploreCategories">
-                    <Link to="/category/rent">
+                <p className='exploreCategoryHeading'>Categories</p>
+                <div className='exploreCategories'>
+                    <Link to='/category/rent'>
                         <img
                             src={rentCategoryImage}
-                            alt="rent"
-                            className="exploreCategoryImg"
+                            alt='rent'
+                            className='exploreCategoryImg'
                         />
-                        <p className="exploreCategoryName">Places for rent</p>
+                        <p className='exploreCategoryName'>Places for rent</p>
                     </Link>
-                    <Link to="/category/sale">
+                    <Link to='/category/sale'>
                         <img
                             src={sellCategoryImage}
-                            alt="sell"
-                            className="exploreCategoryImg"
+                            alt='sell'
+                            className='exploreCategoryImg'
                         />
-                        <p className="exploreCategoryName">Places for sell</p>
+                        <p className='exploreCategoryName'>Places for sell</p>
                     </Link>
                 </div>
             </main>
